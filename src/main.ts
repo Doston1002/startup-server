380884f8842ed56d4cd6d391b6d9fa94cb091aed
doston1002 <polatovdoston1002@gmail.com>
2025-06-19 10:40:07 +0500

    backend

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: [
       'http://localhost:3000',
       'https://uyda-talim.uz',
-      'http://213.230.99.101:2246',
     ],
     methods: ["GET", "POST"],
     credentials: true,
