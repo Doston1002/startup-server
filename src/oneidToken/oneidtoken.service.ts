9b34dcd40dfea9bd8afc7e1b65cec7c96d8e2b74
doston1002 <polatovdoston1002@gmail.com>
2025-08-27 10:27:02 +0500

    get token

--- src/oneidToken/oneidtoken.service.ts
+++ src/oneidToken/oneidtoken.service.ts
@@ -9,21 +9,22 @@ export class OneIdService {
   async exchangeCodeForToken(code: string) {
     const params = new URLSearchParams();
     params.append('grant_type', 'one_authorization_code');
-    params.append('code', code);
     params.append('client_id', process.env.ONEID_CLIENT_ID!);
     params.append('client_secret', process.env.ONEID_CLIENT_SECRET!);
     params.append('redirect_uri', process.env.ONEID_REDIRECT_URI!);
-
+    params.append('code', code);
+  
     const { data } = await this.httpService.axiosRef.post(
-      process.env.ONEID_TOKEN_URL!,
-      params,
+      process.env.ONEID_AUTH_URL!, // ⚠️ Token.do emas, hujjat bo‘yicha Authorization.do
+      params.toString(),
       {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       },
     );
-
+  
     return data; // { access_token, refresh_token, expires_in, ... }
   }
+  
 
   // TOKEN -> USER INFO
   async getUserInfo(accessToken: string) {
