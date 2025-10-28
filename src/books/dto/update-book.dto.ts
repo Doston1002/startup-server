8a0344f3afe1b37e10e0ce70c05b6e53a06331b9
doston1002 <polatovdoston1002@gmail.com>
2025-10-28 20:00:14 +0500

    test

--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,4 +1,22 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateBookDto } from './create-book.dto';
+import { IsString, IsNotEmpty, MinLength, MaxLength, IsOptional } from 'class-validator';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(3, { message: 'Kitob nomi kamida 3 belgidan iborat bo\'lishi kerak' })
+  @MaxLength(200, { message: 'Kitob nomi 200 belgidan oshmasligi kerak' })
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+
+  @IsOptional()
+  @IsString()
+  pdf?: string;
+
+  @IsOptional()
+  @IsString()
+  category?: string;
+}
